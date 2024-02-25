9890169c5ff9864b5cdedd90ba7f5e1b84ead856
Albert Mends <mendsalbert@gmail.com>
2024-02-25 21:22:34 +0000

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,11 @@ import {
   ProductItem,
 } from "@/components/ui/NavBar";
 import { Button } from "@/components/ui/MovingBorder";
+import {
+  GlowingStarsBackgroundCard,
+  GlowingStarsDescription,
+  GlowingStarsTitle,
+} from "@/components/ui/GlowingStars";
 
 export default function Home() {
   return (
@@ -55,6 +60,20 @@ export default function Home() {
             of copy.
           </p>
         </div>
+      </div>
+
+      <div className="flex py-20 items-center justify-center antialiased">
+        <GlowingStarsBackgroundCard>
+          <GlowingStarsTitle>Next.js 14</GlowingStarsTitle>
+          <div className="flex justify-between items-end">
+            <GlowingStarsDescription>
+              The power of full-stack to the frontend. Read the release notes.
+            </GlowingStarsDescription>
+            <div className="h-8 w-8 rounded-full bg-[hsla(0,0%,100%,.1)] flex items-center justify-center">
+              <Icon />
+            </div>
+          </div>
+        </GlowingStarsBackgroundCard>
       </div>
     </div>
   );
@@ -115,3 +134,22 @@ function Navbar({ className }: { className?: string }) {
     </div>
   );
 }
+
+const Icon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className="h-4 w-4 text-white stroke-2"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+      />
+    </svg>
+  );
+};
