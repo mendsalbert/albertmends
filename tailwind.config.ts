e244caf1cd418613fc184090433d6513738a33ba
Albert Mends <mendsalbert@gmail.com>
2024-02-28 12:12:18 +0000

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,7 @@ const config: Config = {
       },
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
+        "meteor-effect": "meteor 5s linear infinite",
       },
       keyframes: {
         spotlight: {
@@ -32,6 +33,14 @@ const config: Config = {
           "100%": {
             opacity: 1 as any,
             transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
           },
         },
       },
